5e03f4f68e6cd0924094c3ae5ee8ab14b8cd90ab
Una Thompson <una@unascribed.com>
2021-01-28 16:28:16 -0800

    I didn't want to do it this way but Forge forced me to
    Fixes #11

--- src/main/java/com/unascribed/notenoughcreativity/mixin/MixinLightmapTextureManager.java
+++ src/main/java/com/unascribed/notenoughcreativity/mixin/MixinLightmapTextureManager.java
@@ -2,22 +2,21 @@ package com.unascribed.notenoughcreativity.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.At.Shift;
-
+import org.spongepowered.asm.mixin.injection.Redirect;
 import com.unascribed.notenoughcreativity.Ability;
 import com.unascribed.notenoughcreativity.AbilityCheck;
 
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.options.GameOptions;
 import net.minecraft.client.render.LightmapTextureManager;
 
 @Mixin(LightmapTextureManager.class)
 public class MixinLightmapTextureManager {
 
-	@ModifyVariable(at=@At(value="FIELD", target="net/minecraft/client/options/GameOptions.gamma:D", shift=Shift.BY, by=3),
-			method="update(F)V", ordinal=5)
-	public float modifyGamma(float orig) {
-		return AbilityCheck.enabled(MinecraftClient.getInstance().player, Ability.NIGHTVISION) ? 200 : orig;
+	@Redirect(at=@At(value="FIELD", target="net/minecraft/client/options/GameOptions.gamma:D"),
+			method="update(F)V")
+	public double modifyGamma(GameOptions subject) {
+		return AbilityCheck.enabled(MinecraftClient.getInstance().player, Ability.NIGHTVISION) ? 200 : subject.gamma;
 	}
 	
 }
